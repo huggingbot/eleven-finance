69b57837823a6952644655922549422ea07d95fd
huggingrobot <huggingrobot@gmail.com>
2021-05-21 21:13:09 +0800

    Updates

---
+++ precache-manifest.0d1c29d9b1567ad1379e838bf95cae86.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9766eff6f7f936d0d32be087bf9cc2eb",
+    "url": "/paniniswap/index.html"
+  },
+  {
+    "revision": "52a8745f3d6fd9fdb7fb",
+    "url": "/paniniswap/static/css/main.2e5a752b.chunk.css"
+  },
+  {
+    "revision": "842074f1e2cc4e8d95db",
+    "url": "/paniniswap/static/js/2.a68ef3ee.chunk.js"
+  },
+  {
+    "revision": "7a1a92514134590a69c32914f1d23992",
+    "url": "/paniniswap/static/js/2.a68ef3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a8745f3d6fd9fdb7fb",
+    "url": "/paniniswap/static/js/main.0dc7e267.chunk.js"
+  },
+  {
+    "revision": "ceb82aa81bb6dcbdaf5c",
+    "url": "/paniniswap/static/js/runtime-main.3432e190.js"
+  },
+  {
+    "revision": "a95ba92605dc62359e998c12a58ebe83",
+    "url": "/paniniswap/static/media/11-logo.a95ba926.png"
+  },
+  {
+    "revision": "387d8122509c1b0707c94d4f5e1bb94a",
+    "url": "/paniniswap/static/media/ELE-logo.387d8122.svg"
+  },
+  {
+    "revision": "34f25f68a9c417b7b29b93021940aa01",
+    "url": "/paniniswap/static/media/NINI-AVAX LP-logo.34f25f68.svg"
+  },
+  {
+    "revision": "a95ba92605dc62359e998c12a58ebe83",
+    "url": "/paniniswap/static/media/NINI-logo.a95ba926.png"
+  },
+  {
+    "revision": "b24396917a138794160b24ff04a71d80",
+    "url": "/paniniswap/static/media/WorkSans-VariableFont_wght.b2439691.ttf"
+  },
+  {
+    "revision": "9f17b8f64f847716b510f89546f3da39",
+    "url": "/paniniswap/static/media/clint-mckoy.9f17b8f6.jpg"
+  }
+]);
